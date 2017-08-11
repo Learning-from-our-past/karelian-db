702d0c1586a74c6bbeb129e12e7cbc290eda6615
Tuomas Salmi <salmi.tuomas@gmail.com>
2017-08-11 20:20:45 +0300

    Drop connections to the db before truncate command

--- tasks/truncate.sh
+++ tasks/truncate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+psql -U postgres -d learning-from-our-past -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'learning-from-our-past' AND pid <> pg_backend_pid();"
 dropdb learning-from-our-past -U postgres
 createdb learning-from-our-past -U postgres
 psql -U postgres -d learning-from-our-past -f sql/initial_db.sql
