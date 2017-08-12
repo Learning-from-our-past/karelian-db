7dbd124dbec9ba67c40ca7331ae650da6b965dab
Tuomas Salmi <salmi.tuomas@gmail.com>
2017-08-12 12:08:00 +0300

    Rename parameters

--- db_management/update_database.py
+++ db_management/update_database.py
@@ -58,11 +58,11 @@ def _get_field_from_json(collection, path, index):
 def _map_data_to_model(model, other_models, mapping_operations, extra_data=None, index=None):
 
     for key, field_details in mapping_operations['mappings'].items():
-        result_of_operation = _get_field_from_json(other_models, field_details['json_path'], index)
+        result_of_operation = _get_field_from_json(data_entry, field_details['json_path'], index)
 
         if model.get_editable_fields() is None or key in model.get_editable_fields():
             for op in field_details['operations']:
-                model, result_of_operation = op(key, model, result_of_operation, other_models, extra_data)
+                model, result_of_operation = op(key, model, result_of_operation, data_entry, extra_data)
 
     return model
 
