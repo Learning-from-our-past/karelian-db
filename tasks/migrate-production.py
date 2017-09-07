0bd202a1974cfe4556315055d7be6859ce62abaa
Tuomas Salmi <salmi.tuomas@gmail.com>
2017-09-07 20:24:06 +0300

    Update peewee_migrate and pass schema as parameter
    Library was updated and now schema can be passed as Router param instead
    of having to dig into MigrateHistory  model.

--- tasks/migrate-production.py
+++ tasks/migrate-production.py
@@ -1,14 +1,9 @@
 from peewee_migrate import Router
 from playhouse.db_url import connect
 
-# FIXME: Dirty way to pass the schema for migration history so that peewee_migrate will work properly.
-# More information: https://github.com/klen/peewee_migrate/issues/51
-from peewee_migrate import MigrateHistory
-MigrateHistory._meta.schema = 'system'
-
 database = connect('postgresql://postgres@karelia-17.it.helsinki.fi:5432/learning-from-our-past')
 
-router = Router(database)
+router = Router(database, schema='system')
 
 # Run all unapplied migrations
 router.run()
