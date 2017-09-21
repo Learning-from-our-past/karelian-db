48686abf54ea57731f3b8761258a94154d212858
Tuomas Salmi <salmi.tuomas@gmail.com>
2017-09-21 11:57:28 +0300

    Populate person data to db in kairatools tests

--- kairatools/tests/conftest.py
+++ kairatools/tests/conftest.py
@@ -1,4 +1,9 @@
 import pytest
+from database.db_management.models.db_connection import db_connection
+import database.tests.utils.population_utils as population_utils
+from database.tests.test_config import CONFIG
+import database.config as config
+from database.tests.utils.dbUtils import DBUtils
 
 
 def pytest_collection_modifyitems(session, config, items):
@@ -14,3 +19,18 @@ def pytest_collection_modifyitems(session, config, items):
                 items.clear()
                 found_only_marker = True
             items.append(item)
+
+
+@pytest.yield_fixture(autouse=True, scope='function', name='person_data')
+def populate_person_information_to_db():
+    # Note this uses connection and utils from database/ project
+    DBUtils.init_test_db()
+    db_connection.init_database(CONFIG['test_db_name'], CONFIG['db_user'])
+    db_connection.connect()
+    database = db_connection.get_database()
+
+    config.CONFIG['anonymize'] = False
+    DBUtils.truncate_db()
+    data = population_utils.populate_from_json(database, "./database/tests/populate/data/person2.json")
+    db_connection.close()
+    return data
