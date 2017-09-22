5a9fb8728f1d27e38f8237be962c8b80972fe68d
Tuomas Salmi <salmi.tuomas@gmail.com>
2017-09-22 22:09:51 +0300

    Add fixme

--- common/testing/population_utils.py
+++ common/testing/population_utils.py
@@ -1,4 +1,6 @@
 import json
+# FIXME: These dependencies should be injected to this module's functions
+# instead of making this module dependent of database module!
 from database.db_management.update_report import update_report
 from database.main import populate_db
 
