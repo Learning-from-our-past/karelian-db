bb0a6125afaf552a380af26d4959d9a354d33ac9
Tuomas Salmi <salmi.tuomas@gmail.com>
2017-10-04 11:22:29 +0300

    Replace " with ' in test configs

--- common/testing/database_test_config.py
+++ common/testing/database_test_config.py
@@ -1,13 +1,13 @@
 CONFIG = {
-  "drop_database_on_init": True,
-  "master_db": "postgres",
-  "test_db_name": "karelian_testdb",
-  "admin_user": "postgres",
-  "db_user": "kaira",
-  "db_admin_user": "postgres",
-  "sql_files": [
-    "./database/sql/initial_db.sql",
-    "./database/tests/test_users.sql"
+  'drop_database_on_init': True,
+  'master_db': 'postgres',
+  'test_db_name': 'karelian_testdb',
+  'admin_user': 'postgres',
+  'db_user': 'kaira',
+  'db_admin_user': 'postgres',
+  'sql_files': [
+    './database/sql/initial_db.sql',
+    './database/tests/test_users.sql'
   ],
   'database_migration_path': 'database/migrations',
   'kairatools_migration_path': 'kairatools/backend/migrations'
