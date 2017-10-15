219f63b4e013add535927f567dbecedd1780fc8b
Tuomas Salmi <salmi.tuomas@gmail.com>
2017-10-15 14:25:24 +0300

    Add first clickable card to dashboard
    Just a Bootstrap card which acts as a Link. Consider
    fancier styling and layout more once there is more actual content in the view.

--- kairatools/frontend/src/dashboard/components/dashboard_index.js
+++ kairatools/frontend/src/dashboard/components/dashboard_index.js
@@ -1,12 +1,31 @@
+import './dashboard_index.css';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class DashboardIndex extends Component {
   render() {
     return (
-      <div>
-        <h2>Content will be here</h2>
-        <p>Something tremendouss</p>
+      <div className="row">
+        <div className="col-8">
+          <h1 className="dashboard-header">Dashboard</h1>
+
+          {/*TODO: These cards should likely be components and generated from some data structure based on user rights*/}
+          <div className="row">
+            <div className="col-6">
+              <div className="card">
+                <Link to="/download" className="card-body tool-card">
+                  <h4 className="card-title">Download data</h4>
+                  <h6 className="card-subtitle mb-2 text-muted">Download data in CSV format</h6>
+                </Link>
+              </div>
+            </div>
+            <div className="col-6">
+            </div>
+          </div>
+        </div>
+        <div className="col-4">
+        </div>
       </div>
     )
   }
