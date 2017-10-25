bbaf71d671e66f64b258ba7bb8a8e612c6cd080c
Tuomas Salmi <salmi.tuomas@gmail.com>
2017-10-25 13:00:30 +0300

    Refactor migrate tasks to be usable with arguments
    Migrate tasks can be imported as regular Python modules and offer a migrate
    function which lets to specify db user name and password to be used in
    migration. Use this in backup restoration script.
    
    Old make migration tasks still work by running the Python module
    directly.

--- database/tasks/migrate.py
+++ database/tasks/migrate.py
@@ -3,11 +3,19 @@ from peewee_migrate import Router
 from common.database_config import CONFIG
 from common.db_connection import db_connection
 
-db_connection.init_database(db_name=CONFIG['db_name'], db_user=CONFIG['db_admin'])
-db_connection.connect()
-database = db_connection.get_database()
 
-router = Router(database, schema='system', migrate_dir='database/migrations')
+def migrate(superuser=None, password=None):
+    db_connection.init_database(db_name=CONFIG['db_name'], db_user=superuser or CONFIG['db_admin'], password=password)
+    db_connection.connect()
+    database = db_connection.get_database()
 
-# Run all unapplied migrations
-router.run()
+    router = Router(database, schema='system', migrate_dir='database/migrations')
+
+    # Run all unapplied migrations
+    router.run()
+
+    database.close()
+
+
+if __name__ == '__main__':
+    migrate()
