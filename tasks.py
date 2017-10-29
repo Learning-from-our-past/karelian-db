cfacded6d9f111ed8612098a88fc57e35b40fad1
Tuomas Salmi <salmi.tuomas@gmail.com>
2017-10-29 11:40:02 +0200

    Recreate database with invoke task

--- tasks.py
+++ tasks.py
@@ -21,14 +21,7 @@ def test_all(ctx):
     test(ctx, 'kairatools/backend')
 
 
-@task(help={'superuser': 'The database super user which can be used to create and modify the database.'})
-def setup(ctx, superuser='postgres'):
-    """
-    Setup database and run initialization and migrations.
-    To avoid repetitive password prompts, it is suggested to have a pgpass-entry for superuser
-    used in this task.
-    """
-    ctx.run("psql -U {} template1 -c 'create extension hstore';".format(superuser))
+def _setup_database(ctx, superuser):
     ctx.run('createdb -U {} learning-from-our-past'.format(superuser))
     ctx.run('psql -U {} -d learning-from-our-past -a -f database/sql/initial_db.sql'.format(superuser))
 
@@ -37,3 +30,24 @@ def setup(ctx, superuser='postgres'):
     migrate_kairatools(superuser, superuser_password)
     ctx.run('python -m database.tasks.migrate')
     ctx.run('python -m kairatools.backend.tasks.migrate')
+
+
+@task(help={'superuser': 'The database super user which can be used to create and modify the database.'})
+def setup(ctx, superuser='postgres'):
+    """
+    Setup database and run initialization and migrations.
+    To avoid repetitive password prompts, it is suggested to have a pgpass-entry for superuser
+    used in this task.
+    """
+    ctx.run("psql -U {} template1 -c 'create extension hstore';".format(superuser))
+    _setup_database(ctx, superuser)
+
+
+@task(help={'superuser': 'The database super user which can be used to create and modify the database.'})
+def recreate_db(ctx, superuser='postgres'):
+    """
+    Drops the existing database and recreates it from scratch.
+    """
+    ctx.run('dropdb -U {} learning-from-our-past'.format(superuser))
+    _setup_database(ctx, superuser)
+
