848909f41d1bfec021bfb2df67c87517866fd849
Tuomas Salmi <salmi.tuomas@gmail.com>
2017-10-29 19:36:57 +0200

    Populate data to database by invoke command
    Both first and all books can be populated to the db via
    invoke. Did not create update-production task since it is never
    used anyway.

--- database/tasks/__init__.py
+++ database/tasks/__init__.py
@@ -32,3 +32,32 @@ def migrate(ctx, production=None):
         migrate_production()
     else:
         migrate_local()
+
+
+@task(optional=['first', 'all_books'], help={
+    'first': 'Populate only first book to the database.',
+    'all_books': 'Populates all siirtokarjalaisten_tie json files form material/ directory'
+})
+def populate(ctx, first=None, all_books=None):
+    """
+    Populate json files in material directory to the database. Either -f or -a
+    arguments should be provided.
+    """
+    # TODO: Maybe support providing a relative path to any file? However, note the slightly strange path, since working
+    # directory is changed to parent directory...
+    karelian_books = [
+        'siirtokarjalaiset_I.json',
+        'siirtokarjalaiset_II.json',
+        'siirtokarjalaiset_III.json',
+        'siirtokarjalaiset_IV.json',
+    ]
+
+    os.chdir('../')
+    if first:
+        ctx.run('python -m database.main database/material/{}'.format(karelian_books[0]))
+    elif all_books:
+        for book in karelian_books:
+            ctx.run('python -m database.main database/material/{}'.format(book))
+    else:
+        print('Should provide either [first], or [all] flag on invocation!')
+        sys.exit(1)
