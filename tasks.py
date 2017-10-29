d854c7699021c2debd15ea342ae77ac9b7b38c5e
Tuomas Salmi <salmi.tuomas@gmail.com>
2017-10-29 11:35:19 +0200

    Setup database with invoke task
    Allows to define the superuser in task parameter which should prove handy
    in production environments.

--- tasks.py
+++ tasks.py
@@ -1,4 +1,7 @@
 from invoke import task
+import getpass
+from database.tasks.migrate import migrate as migrate_backend
+from kairatools.backend.tasks.migrate import migrate as migrate_kairatools
 
 
 @task()
@@ -16,3 +19,21 @@ def test_all(ctx):
     """
     test(ctx, 'database')
     test(ctx, 'kairatools/backend')
+
+
+@task(help={'superuser': 'The database super user which can be used to create and modify the database.'})
+def setup(ctx, superuser='postgres'):
+    """
+    Setup database and run initialization and migrations.
+    To avoid repetitive password prompts, it is suggested to have a pgpass-entry for superuser
+    used in this task.
+    """
+    ctx.run("psql -U {} template1 -c 'create extension hstore';".format(superuser))
+    ctx.run('createdb -U {} learning-from-our-past'.format(superuser))
+    ctx.run('psql -U {} -d learning-from-our-past -a -f database/sql/initial_db.sql'.format(superuser))
+
+    superuser_password = getpass.getpass('Please input password for superuser {}: '.format(superuser))
+    migrate_backend(superuser, superuser_password)
+    migrate_kairatools(superuser, superuser_password)
+    ctx.run('python -m database.tasks.migrate')
+    ctx.run('python -m kairatools.backend.tasks.migrate')
