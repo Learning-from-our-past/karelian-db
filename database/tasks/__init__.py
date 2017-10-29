ec45f54006d63f655357df5315f0577d99f4fe10
Tuomas Salmi <salmi.tuomas@gmail.com>
2017-10-29 19:17:12 +0200

    Execute migration tasks with invoke
    Had to refactor tasks a bit so that they could be called from
    Python code. Now migration to local and production environments
    can be done with same task depending on command line flag.

--- database/tasks/__init__.py
+++ database/tasks/__init__.py
@@ -1,5 +1,12 @@
 from invoke import task
 import os
+import sys
+# A slight hack to change the module path to parent directory so that all imports work as expected even when these
+# tasks are ran inside database/ directory.
+sys.path[0] = '../'
+
+from database.tasks.new_migration import create_migration_file
+from database.tasks.migrate import migrate_local, migrate_production
 
 
 @task()
@@ -7,3 +14,21 @@ def test(ctx):
     os.chdir('../')
     ctx.run('python -m pytest database')
 
+
+@task()
+def new_migration(ctx):
+    """
+    Create a new empty migration file to migrations directory.
+    """
+    create_migration_file('migrations')
+
+
+@task(optional=['production'], help={'production': 'If set, runs migrations to the production server instead of local.'})
+def migrate(ctx, production=None):
+    """
+    Run migrations to the database. Default to localhost or production if invoked with option -p.
+    """
+    if production:
+        migrate_production()
+    else:
+        migrate_local()
