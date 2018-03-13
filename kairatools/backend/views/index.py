80b7496dfd4affb555376476dbe04eced78e615f
Juuso Kallioniemi <jkallioniemi@gmail.com>
2018-03-13 11:28:27 +0200

    Delete kairatools directory

--- kairatools/backend/views/index.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template, Blueprint
-
-index_bp = Blueprint('index', __name__, template_folder='templates')
-
-@index_bp.route('/')
-def index():
-    return render_template('index.html')
