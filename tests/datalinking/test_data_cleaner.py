c83b4f5637df5f65db882b2b2f12a0a087bb09e0
Juuso Kallioniemi <jkallioniemi@gmail.com>
2018-03-20 16:16:40 +0200

    Add a DataCleaner that cleans up rows from the DB
    The data needs to be massaged a little bit before it is ready
    for the data linking process. In this commit we add the basic
    processing required for db_id, event_ids, date_of_birth as well
    as the more involved first name normalization.

---
+++ tests/datalinking/test_data_cleaner.py
@@ -0,0 +1,36 @@
+import pytest
+from collections import OrderedDict
+from datalinking.data_cleaner import KatihaDataCleaner
+from datalinking.data_cleaner import katiha_person_cleaned
+
+
+class TestKatihaDataCleaner:
+    @pytest.fixture(autouse=True, scope='class')
+    def cleaner(self):
+        return KatihaDataCleaner()
+
+    @pytest.fixture(autouse=True, scope='class')
+    def data(self):
+        # This data is completely made up, but it has to look real (i.e. names and parish id)
+        # so that the table lookups and name normalizations
+        d = OrderedDict()
+        d['ID'] = 1
+        d['eventId'] = '0504L024b0000431'
+        d['firstName'] = 'Tuomas           '
+        d['secondName'] = 'Juuso               '
+        d['lastName'] = 'Salmi                       '
+        d['birthParish'] = '                              '
+        d['birthDay'] = 20
+        d['birthMonth'] = 5
+        d['birthYear'] = 1930
+        d['parishId'] = '0504'
+        d['motherLanguage'] = ' '
+        return d
+
+    def should_correctly_return_cleaned_person_object(self, cleaner, data):
+        expected_data = katiha_person_cleaned(db_id=data['ID'],
+                                              event_ids=(data['eventId'],),
+                                              normalized_first_names=('thomas', 'joseph'),
+                                              date_of_birth=(data['birthDay'], data['birthMonth'], data['birthYear']))
+        cleaned_data = cleaner.clean_db_rows(data.values())
+        assert cleaned_data == expected_data
