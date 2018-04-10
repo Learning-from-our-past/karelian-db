092084c876e7563766182fcc19ab02f93b0fa122
Juuso Kallioniemi <jkallioniemi@gmail.com>
2018-04-10 10:41:13 +0300

    Add command to run linked data population
    Updated the population code in main.py to use argparse instead
    of sys.argv and removed some dead code to run population from
    testset.json. The code would never execute because it was set
    up to execute if file was not specified, but the way argparse
    is set up is that it requires file to be specified.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 import json
 import os
 import sys
+import pickle
 
 import db_management.siirtokarjalaistentie_models as db_siirtokarjalaistentie_models
 from db_management import katiha_models
@@ -11,12 +12,14 @@ from db_management.csv_record import CsvRecordOfPopulation
 from db_management.mark_ambiguous_region_places_in_db import mark_ambiguous_places
 from db_management.update_database import update_data_in_db
 from db_management.update_report import update_report
+from datalinking.data_populater import populate_linked_data
 
 parser = argparse.ArgumentParser(description='Populate data to the database from json files.')
 parser.add_argument('-a', nargs='?', type=str, help='Host address to the database', default='localhost')
 parser.add_argument('-p', nargs='?', type=int, help='Port of the database', default=5432)
 parser.add_argument('-u', nargs='?', type=str, help='User name. Password should be stored in pgpassfile', default=CONFIG['db_user'])
 parser.add_argument('-d', nargs='?', type=str, help='Database name', default=CONFIG['db_name'])
+parser.add_argument('-t', nargs=1, type=str, help='Data type to populate')
 parser.add_argument('file', metavar='file', type=str, nargs=1, help='The file to populate data from')
 
 def load_json(path):
@@ -24,6 +27,10 @@ def load_json(path):
         data = json.load(data_file)
     return data
 
+def load_pickle(path):
+    with open(path, mode='rb') as data_file:
+        data = pickle.load(data_file)
+    return data
 
 def print_progress(current, max):
     percentage = round((current / max) * 100)
@@ -70,18 +77,15 @@ if __name__ == "__main__":
     db_siirtokarjalaistentie_models.set_database_to_models(database)
     katiha_models.set_database_to_models(database)
 
-    csv_record = None
+    data_type = args['t'][0].casefold()
+    file_name = os.path.splitext(args['file'][0])[0]
+    update_report.setup(file_name)
 
-    if len(sys.argv) > 1:
-        file_name = os.path.splitext(sys.argv[1])[0]
-        update_report.setup(file_name)
+    if data_type == 'kaira':
         csv_record = CsvRecordOfPopulation(file_name)
-        data = load_json(sys.argv[1])
-    else:
-        data = load_json("./material/testset.json")
-        csv_record = CsvRecordOfPopulation('./material/testset')
-
-    populate_db(database, data, csv_record)
-    csv_record.save_to_file()
-
-
+        data = load_json(args['file'][0])
+        populate_db(database, data, csv_record)
+        csv_record.save_to_file()
+    elif data_type == 'link':
+        data = load_pickle(args['file'][0])
+        populate_linked_data(database, data)
