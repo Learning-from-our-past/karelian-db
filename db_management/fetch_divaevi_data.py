4fd9059dc699ab152ff3e22311f929b9f088992f
Jan Böhmeke <jebohm@utu.fi>
2022-11-15 11:21:10 +0200

    Add functionality to fetch DVV data

---
+++ db_management/fetch_divaevi_data.py
@@ -0,0 +1,27 @@
+from db_management.siirtokarjalaistentie_models import *
+
+
+def _fetch_primary_person(kaira_id):
+    try:
+        return Person.get(Person.kairaId == link_kaira_id)
+    except Person.DoesNotExist:
+        return Person()
+
+
+def _fetch_spouse_person(kaira_id):
+    try:
+        return Person.get(Person.kairaId == kaira_id)
+    except Person.DoesNotExist:
+        return Person()
+
+
+def fetch_existing_data_of_person_entry(person_entry):
+    primary_person = _fetch_primary_person(person_entry['primaryPerson']['kairaId'])
+    spouse_person = None
+    if person_entry['spouse']:
+        spouse_person = _fetch_primary_person(person_entry['spouse']['kairaId'])
+
+    return {
+        'primary_person': primary_person,
+        'spouse_person': spouse_person
+    }
